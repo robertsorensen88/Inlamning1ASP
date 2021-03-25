80d208ff49827cb7e41f7d51aff0225cd586207b
Robert Sörensen <robert.sorensen44@gmail.com>
2021-03-25 21:18:26 +0100

    Fixed buttons. TODO Index & button on onPost joinEvent

---
+++ Inlaming1ASP/Pages/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Inlamning1ASP.Data;
+using Inlamning1ASP.Models;
+
+namespace Inlamning1ASP.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly EventsDbContext _context;
+
+        public DetailsModel(EventsDbContext context)
+        {
+            _context = context;
+        }
+
+        public Event Event { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Event = await _context.Events.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Event == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
