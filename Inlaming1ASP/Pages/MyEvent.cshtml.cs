88a171a0f3fd94230a3f5a5bfe878ac54207d4eb
Robert Sörensen <robert.sorensen44@gmail.com>
2021-03-25 10:35:10 +0100

    får ut MyEvent. TODO länka samman allt

---
+++ Inlaming1ASP/Pages/MyEvent.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Inlamning1ASP.Data;
+using Inlamning1ASP.Models;
+
+namespace Inlamning1ASP.Pages
+{
+    public class MyEventModel : PageModel
+    {
+        private readonly EventsDbContext _context;
+
+        public MyEventModel(EventsDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<JoinEvent> JoinEvent { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            JoinEvent = await _context.JoinEvents
+                .Include(r => r.Attendee)
+                .Include(r => r.Event).ToListAsync();
+        }
+    }
+}
